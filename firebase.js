ef4f9d6ab12d4447e44a96e444cf2ac3c237cff2
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2022-02-01 01:13:57 +0600

    firebase user log in

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+       
+var firebaseConfig = {
+    apiKey: "AIzaSyC-0oa6__tLB6Li2XhtYZMKy81lAwEFV0A",
+    authDomain: "prescription-1b85c.firebaseapp.com",
+    projectId: "prescription-1b85c",
+    storageBucket: "prescription-1b85c.appspot.com",
+    messagingSenderId: "98292952447",
+    appId: "1:98292952447:web:91526e578eddb8828dbe4a",
+    measurementId: "G-W1TRP6S0VY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+        
+        
+        
+     
---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById("login-form").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("welcome.html")
+    }
+})
+
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        document.getElementById("error").innerHTML = error.message
+    })
+}
